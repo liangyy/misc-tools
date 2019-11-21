e9aecf45e5a3ac13fccd59c714930e34ac6e9174
Yanyu Liang <yanyul@uchicago.edu>
2019-11-20 22:12:22 -0600

    debugged and added test data

--- liftover_snp.py/liftover_snp.py
+++ liftover_snp.py/liftover_snp.py
@@ -1,6 +1,6 @@
 import argparse
-parser = argparse.ArgumentParser(prog='map2gtexv8.py', description='''
-    Lift over from hg37 to hg38 
+parser = argparse.ArgumentParser(prog='liftover_snp.py', description='''
+    Lift over from one genome build to another
 ''')
 parser.add_argument('--input', help='''
     input file name
@@ -17,17 +17,33 @@ parser.add_argument('--liftover_chain', help='''
 parser.add_argument('--out_txtgz', help='''
     output txt.gz file name
 ''')
+parser.add_argument('--input_delim', type = str, default = 'tab', help='''
+    separator in input, space or tab
+''')
+parser.add_argument('--if_with_chr', default = 0, type = int, help='''
+    if you want to add chr before chromosome, e.g. chr1 instead of 1, then set it to 1
+''')
 args = parser.parse_args()
 
 import pandas as pd
 from lib import liftover
+import re
 
+sep = '\t'
+if args.input_delim == 'space':
+    sep = ' '
+elif args.input_delim == 'tab':
+    sep = '\t'
+else:
+    sep = args.input_delim
 
-df = pd.read_table(args.input)
+df = pd.read_table(args.input, sep = sep)
 
 chr = df.iloc[:, args.chr_col - 1]
 pos = df.iloc[:, args.pos_col - 1] 
 out = liftover(chr, pos, args.liftover_chain)
 df = pd.concat([df.reset_index(drop=True), out], axis=1)
+if args.if_with_chr != 1:
+    df['liftover_chr'] = df['liftover_chr'].apply(lambda x: re.sub('chr', '', x))
 df.to_csv(args.out_txtgz, index = False, sep='\t', compression = 'gzip')
 
