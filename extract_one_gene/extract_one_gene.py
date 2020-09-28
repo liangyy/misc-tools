18b6ad10c5ba2cbeaf1c15a6d0494d67f67c2aca
Yanyu Liang <yanyul@uchicago.edu>
2020-09-28 17:59:23 -0500

    super delayed

--- extract_one_gene/extract_one_gene.py
+++ extract_one_gene/extract_one_gene.py
@@ -8,7 +8,7 @@ parser.add_argument('--input', help='''
 parser.add_argument('--gene_id', type = str, help='''
     gene id
 ''')
-parser.add_argument('--output', type = int, default = 0, help='''
+parser.add_argument('--output', default = 0, help='''
     output csv
 ''')
 args = parser.parse_args()
@@ -19,15 +19,19 @@ import pandas as pd
 
 with h5py.File(args.input, 'r') as f:
     genes = f['genes'][:].astype(str)
-    gene_idx = np.where(np.isin(f, args.gene_id))
+    gene_idx = np.where(np.isin(genes, args.gene_id))[0]
 
-if len(gene_idx) == 0:
+
+# breakpoint()
+if gene_idx.shape[0] == 0:
     print('No {} in the input'.format(args.gene_id))
-elif len(gene_idx) == 1:
+elif gene_idx.shape[0] == 1:
+    # breakpoint()
     gene_idx = gene_idx[0]
     with h5py.File(args.input, 'r') as f:
         pred_expr = f['pred_expr'][gene_idx, :]
         eid = f['samples'][:].astype(str)
+    # breakpoint()
     df = pd.DataFrame({'eid': eid, args.gene_id: pred_expr})
     df.to_csv(args.output, compression='gzip')
 
