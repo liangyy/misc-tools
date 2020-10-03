23991a3af5b38b14e0690f2b83d864fa1a0ace55
Yanyu Liang <yanyul@uchicago.edu>
2020-10-02 22:06:03 -0500

    added list

--- parquet2table/parquet2tsv.py
+++ parquet2table/parquet2tsv.py
@@ -8,9 +8,25 @@ parser.add_argument('--parquet', required=True, help='''
 parser.add_argument('--output', required=True, help='''
     Path to output file
 ''')
+parser.add_argument('--col_list', nargs='+', default=None, help='''
+    A list of columns to extract.
+    Add extract column names.
+''')
 args = parser.parse_args()
 
 import pandas as pd
 
 table = pd.read_parquet(args.parquet)
+
+if args.col_list is None:
+    pass
+else:
+    cols = args.col_list[1:]
+    list_cols = []
+    with open(args.col_list[0], 'r') as f:
+        for i in f:
+            i = i.strip()
+            list_cols.append(i)
+    table = table[ cols + list_cols ]
+    
 table.to_csv(args.output, compression='gzip', sep='\t', index=False)
