b5d4039c80b5618c6d1ee271dc9bf4dc3cd65f2c
Yanyu Liang <yanyul@uchicago.edu>
2020-10-08 16:13:28 -0500

    added snp chunk

--- bgen_io/ukb_imp_reader.py
+++ bgen_io/ukb_imp_reader.py
@@ -54,6 +54,26 @@ class UKBReader:
         missing = np.isnan(dosage)
         dosage[missing] = np.nanmean(dosage)
         return dosage 
+        
+    def get_dosage_by_chunk(self, snpid_list):
+        '''
+        Extract genotype dosage prob
+        and convert to expected dosage of second allele by
+        dosage[:, 1] + 2 * dosage[:, 2].
+        The samples labelled as missing will be imputed to population mean.
+        '''
+        cached_data = self.rbgen.bgen_load(
+            self.bgen_path,
+            index_filename=self.bgi_path,
+            rsids=StrVector(snpid_list), 
+            max_entries_per_sample=4
+        )
+        dosage = pandas2ri.ri2py(cached_data[4])
+        # dosage: nvariant (we have 1 here) x nsample x num_of_allele_combination
+        dosage = dosage[:, :, 1] + 2 * dosage[:, :, 2]
+        missing = np.where(np.isnan(dosage))[0]
+        dosage[missing[0], missing[1]] = np.nanmean(dosage, axis=1)[missing[0]]
+        return dosage 
 
     def dosage_generator(self, id_list):
         for snpid in id_list:
