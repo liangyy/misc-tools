2a26a1eea2f5823b49253487f1510a95fc65e5e5
Yanyu Liang <yanyul@uchicago.edu>
2020-10-08 11:25:59 -0500

    debugged reader

--- bgen_io/ukb_imp_reader.py
+++ bgen_io/ukb_imp_reader.py
@@ -7,7 +7,7 @@ class UKBReader:
           key being byid = rsid or id; 
           value being the index in genotype file.
         '''
-        self.bgen = bgen_reader.read_bgen(bgen, metafile_filepath=metafile, )
+        self.bgen = bgen_reader.read_bgen(bgen, metafile_filepath=metafile, samples_filepath=sample)
         self.variant_df = self.bgen['variants'].compute() 
         self.samples = self.bgen["samples"].tolist()
         self.byid = byid
@@ -15,27 +15,28 @@ class UKBReader:
         
     def _init_id_dict(self):
         if self.byid == 'rsid':
-            self.id_dict = { variant_df.rsid[i]: i for i in range(variant_df.shape[0]) }
+            self.id_dict = { self.variant_df.rsid[i]: i for i in range(self.variant_df.shape[0]) }
         elif self.byid == 'id':
-            self.id_dict = { variant_df.id[i]: i for i in range(variant_df.shape[0]) }
+            self.id_dict = { self.variant_df.id[i]: i for i in range(self.variant_df.shape[0]) }
         else:
             raise ValueError('Only support rsid and id.')
     
-    def get_dosage_by_id(self, id):
+    def get_dosage_by_id(self, snpid):
         '''
         Extract genotype dosage prob
         and convert to expected dosage of second allele by
         dosage[:, 1] + 2 * dosage[:, 2].
         The samples labelled as missing will be imputed to population mean.
         '''
-        if id not in self.id_dict:
+        if snpid not in self.id_dict:
             # the id is not valid
             return None
         res = self.bgen['genotype'][self.id_dict[snpid]].compute()
         dosage = res['probs']
         dosage = dosage[:, 1] + 2 * dosage[:, 2]
         dosage[res['missing']] = dosage[~res['missing']].mean()
+        return dosage 
 
     def dosage_generator(self, id_list):
-        for id in id_list:
-            yield self.get_dosage_by_id(id)
+        for snpid in id_list:
+            yield self.get_dosage_by_id(snpid)
