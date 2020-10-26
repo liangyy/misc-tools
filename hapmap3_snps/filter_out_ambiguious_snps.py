f0e37a25fc4fdd5679328b5410c3ab1f6047e764
Yanyu Liang <yanyul@uchicago.edu>
2020-10-26 10:13:04 -0500

    added post filter

--- hapmap3_snps/filter_out_ambiguious_snps.py
+++ hapmap3_snps/filter_out_ambiguious_snps.py
@@ -20,11 +20,24 @@ def filter_out_ambiguious_snps(df):
             is_ambi.append(False)
     df = df[np.logical_not(np.array(is_ambi))].reset_index(drop=True)
     return df        
-            
+
+def clean_up_rsid(mylist):
+    o = []
+    for i in mylist:
+        if i.startswith('rs'):
+            o.append(i)
+        elif i.startswith('SNP'):
+            pass
+        elif i.startswith('AFFX-SNP'):
+            i = i.split('__')[-1]
+            o.append(i)
+    return o
+        
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(prog='filter_out_ambiguious_snps.py', description='''
         Filter out ambiguious SNPs.
+        Will keep SNPs with legitimate rsID only.
         CAUTION: Need to have path to misc-tools/pyutil in the PYTHONPATH!
     ''')
     parser.add_argument('--input', help='''
@@ -55,5 +68,12 @@ if __name__ == '__main__':
     
     df_bim = filter_out_ambiguious_snps(df_bim)
     snp_list = intersection(snp_list, list(df_bim.rsid))
+    
+    # before output
+    # I found that there are some SNPs with ID like: AFFX-SNP_XXX__rsYYY or SNP_A-ZZZ
+    # I cleaned them up by removing all SNPs with SNP_A-ZZZ as ID and 
+    # keep rsYYY from AFFX-SNP_XXX__rsYYY 
+    snp_list = clean_up_rsid(snp_list)
+    
     write_list(snp_list, args.output)
     
