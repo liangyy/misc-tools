57343f778017aa2c03c284f0dc19030574a12009
Yanyu Liang <yanyul@uchicago.edu>
2020-12-08 09:41:19 -0600

    added pbs 101

---
+++ pbs_101/README.md
@@ -0,0 +1,4 @@
+Here we have an example on how to submit jobs with parameters in PBS scheduler.
+
+`PBS_JOBNAME` and `PBS_JOBID` are built-in parameters. It will be automatically assigned when your job get submitted.
+
---
+++ pbs_101/submit.sh
@@ -0,0 +1,15 @@
+# Here we submit a group of jobs where we fix parameter2 and parameter3
+# but want to run with a grid of parameter1 values (1 .. 100)
+# also we want to run the same job for all the autosomes
+
+ 
+param1s=`seq 1 100`
+param2=12
+param3=0.1
+
+for chr in `seq 1 22`
+  for param1 in $param1s
+    do
+      qsub -v PARAMETER1=$param1,PARAMETER2=$param2,PARAMETER3=$param3,CHR_NUM=$chr run.pbs
+    done
+done
