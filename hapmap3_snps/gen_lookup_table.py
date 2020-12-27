0841aa30386c5a32bb5174695190ee0997a1cb07
Yanyu Liang <yanyul@uchicago.edu>
2020-12-27 12:13:12 -0600

    updated so that it output metadata format

--- hapmap3_snps/gen_lookup_table.py
+++ hapmap3_snps/gen_lookup_table.py
@@ -1,3 +1,9 @@
+def gen_snpid(chr, pos, a1, a2, build):
+    res = []
+    for x1, x2, x3, x4 in zip(chr, pos, a1, a2):
+        res.append(f'chr{x1}_{x2}_{x3}_{x4}_{build}')
+    return res
+
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(prog='gen_lookup_table.py', description='''
@@ -9,11 +15,18 @@ if __name__ == '__main__':
     parser.add_argument('--input', help='''
         The list of HapMap SNPs.
     ''')
-    parser.add_argument('--input_map', help='''
-        The HapMap plink MAP file.
+    parser.add_argument('--input_bim', help='''
+        The HapMap plink BIM file.
+    ''')
+    parser.add_argument('--input_frq', help='''
+        The HapMap plink FRQ file.
     ''')
     parser.add_argument('--liftover_chain', default=None, help='''
-        If want to liftover to another genome build, specify the chain file.
+        If want to liftover to another genome build, specify the chain file. 
+        And target build tag.
+    ''')
+    parser.add_argument('--target_build', help='''
+        The target genome build of output metadata.
     ''')
     parser.add_argument('--output', help='''
         Output SNP lookup table.
@@ -37,9 +50,13 @@ if __name__ == '__main__':
     logging.info('Loading SNP list.')
     snp_list = load_list(args.input)
     
-    logging.info('Loading SNP MAP file.')
-    df_map = pd.read_csv(args.input_map, sep='\s+', header=None)
-    df_bim.columns = ['chr', 'rsid', 'placeholder', 'pos']
+    logging.info('Loading SNP BIM file.')
+    df_map = pd.read_csv(args.input_bim, sep='\s+', header=None)
+    df_bim.columns = ['chr', 'rsid', 'placeholder', 'pos', 'a1', 'a2']
+    
+    logging.info('Loading SNP FRQ file.')
+    df_frq = pd.read_csv(args.input_frq, sep='\s+', header=None)
+    df_bim = pd.merge(df_bim, df_frq[['CHR', 'SNP', 'MAF']], left_on=['chr', 'rsid'], right_on=['CHR', 'SNP'])
     
     logging.info('Intersecting list and MAP.')
     df_bim = df_bim[ df_bim.rsid.isin(snp_list) ].reset_index(drop=True)
@@ -55,9 +72,17 @@ if __name__ == '__main__':
         df_bim = df_bim[ ~ df_bim.pos.isna() ].reset_index(drop=True)
         logging.info('There are {} SNPs left after liftover.'.format(df_bim.shape[0]))
     
-    df_bim.start = df_bim.pos
-    df_bim.rename(columns={'pos': 'end', 'rsid': 'name'}, inplace=True)
-    df_bim = df_bim[['chr', 'start', 'end', 'name']]
+    df_bim.rename(
+        columns={
+            'pos': 'position', 'chr': 'chromosome', 
+            'a2': 'allele_0', 'a1': 'allele_1', 
+            'allele_1_frequency': 'MAF'
+        }, 
+        inplace=True
+    )
+    df_bim['chromosome'] = df_bim.chromosome.apply(lambda x: re.sub('chr', '', str(x)))
+    df_bim['id'] = gen_snpid(df_bim.chromosome, df_bim.position, df_bim.allele_0, df_bim.allele_1, args.target_build)
+    df_bim = df_bim[['chromosome', 'position', 'id', 'allele_0', 'allele_1', 'allele_1_frequency', 'rsid']]
     
     logging.info('Writing to disk.')
     df_bim.to_csv(args.output, sep='\t', compression='gzip', index=False)
