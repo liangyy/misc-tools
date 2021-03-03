5b0c57572bc8d9c6f2904f726e4d2c863f3bb37e
Yanyu Liang <yanyul@midway2-login2.rcc.local>
2021-03-02 21:00:19 -0600

    some fixes

--- est_polygenicity/scripts/run_sld4m.m
+++ est_polygenicity/scripts/run_sld4m.m
@@ -1,4 +1,4 @@
-function [] = run_sld4m(l2l4_file, gwas_file, out_file, mode)
+function [] = run_sld4m(l2l4_file, gwas_file, out_file, mode_)
 % INPUT:
 % l2l4_file: MAT file
 % gwas_file: MAT file
@@ -11,16 +11,26 @@ load(gwas_file, 'chisq', 'sumstat_RSIDs_as_ints');
 Annot_names = string(header)';
 no_blocks=100;
 [~, idx_ref, idx_sumstat] = intersect(RSIDs, sumstat_RSIDs_as_ints, 'stable');
+fprintf('number of SNPs to use = %d \n', size(idx_ref, 1));
 
-
-if mode == 'simple'
-  ell2 = ell2(:, 1)
-  ell4 = ell4(:, 1)
-  annot = annot(:, 1)
-  Annot_names = Annot_names(1)
+if strcmp(mode_, 'simple')
+  ell2 = ell2(:, 1);
+  ell4 = ell4(:, 1);
+  annot = annot(:, 1);
+  Annot_names = Annot_names(1);
+  report_annot_mat = [ 1 1 ];
+elseif strcmp(mode_, 'all')
+  % looks good
+  report_annot_mat = [ ones(size(ell4, 2), 1), eye(size(ell4, 2)) ];
+else
+  fprintf('Wrong mode = %s', mode_); 
 end
+fprintf('Mode = %s', mode_);
 
-[Ma_est, Ma_err] = SLD4M( chisq(idx_sumstat), ell2, ell4, idx_ref, annot, EW2(idx_ref), EW4(idx_ref), no_blocks);
+[Ma_est, Ma_err] = SLD4M( chisq(idx_sumstat), ell2, ell4, idx_ref, annot, EW2(idx_ref), EW4(idx_ref), no_blocks, 1, report_annot_mat);
 
-out_table = table(Annot_names, Ma_est, Ma_err);
+out_table = table([ "All", Annot_names' ]', Ma_est, Ma_err);
 writetable(out_table, out_file, 'Delimiter', ',');
+
+exit
+
