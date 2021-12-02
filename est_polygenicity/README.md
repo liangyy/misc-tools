0da1cecce4852b76e0a5e84aefe73073accfbb55
Yanyu Liang (UChicago) <yanyuluchicago@Yanyus-MacBook-Pro.local>
2021-12-02 13:47:36 -0800

    added more outputs to sld4m

--- est_polygenicity/README.md
+++ est_polygenicity/README.md
@@ -6,3 +6,18 @@ Approaches:
 * LD4M [ref](https://www.sciencedirect.com/science/article/pii/S0002929719302666)
 * FMR [ref](https://www.biorxiv.org/content/10.1101/2020.09.19.304097v3) Maybe not now ..
  
+# SLD4M running mode
+
+* `base`: no stratification on SNPs.
+* `all`: include all annotations.
+* `all_w_agg`: include all annotations plus an aggregated category by aggregating MAF bins for common variants. 
+
+# Output of SLD4M CSV table
+
+* `*_est`: point estimate, `*_err`: standard error
+* `Ma`: polygenicity Me
+* `h2`: heritability estimate (maybe scaled?)
+* `Maenrich`: estimated Me enrichment (relative to base annotation, i.e. all SNP average)
+* `h2enrich`: estimated heritability enrichment (relative to base annotation, i.e. all SNP average)
+
+
--- est_polygenicity/scripts/run_sld4m.m
+++ est_polygenicity/scripts/run_sld4m.m
@@ -3,7 +3,10 @@ function [] = run_sld4m(l2l4_file, gwas_file, out_file, mode_)
 % l2l4_file: MAT file
 % gwas_file: MAT file
 % out_file: csv format
-% mode: annot = 1 (mode = simple) or annot = all (mode = all)
+% mode: annot = base (mode = base)
+%       annot = all annotations (mode = all)
+%       annot = all annotations with one extra that aggregates common MAF bins 
+%               (mode = all_w_agg)
 
 load(l2l4_file);
 load(gwas_file, 'chisq', 'sumstat_RSIDs_as_ints');
@@ -12,27 +15,49 @@ Annot_names = string(header)';
 no_blocks=100;
 [~, idx_ref, idx_sumstat] = intersect(RSIDs, sumstat_RSIDs_as_ints, 'stable');
 fprintf('number of SNPs to use = %d \n', size(idx_ref, 1));
+fprintf('running mode = %s \n', mode_);
 
-if strcmp(mode_, 'simple')
+if strcmp(mode_, 'base')
   ell2 = ell2(:, 1);
   ell4 = ell4(:, 1);
   annot = annot(:, 1);
   Annot_names = Annot_names(1);
-  report_annot_mat = [ 1 1 ];
-elseif strcmp(mode_, 'all')
-  % looks good
+  report_annot_mat = [ 1 ];
+  ref_idx = 1;
+elseif strcmp(mode_, 'all_w_agg')
+  % add aggregation as the first category
   tmp = zeros(size(ell4, 2), 1);
   tmp(60:69) = 1;
   disp(['Aggregate:', header(60:69)])
   report_annot_mat = [ tmp, eye(size(ell4, 2)) ];
+  ref_idx = 2;
+elseif strcmp(mode_, 'all')
+  report_annot_mat = eye(size(ell4, 2));
+  ref_idx = 1;
 else
   fprintf('Wrong mode = %s', mode_); 
 end
 fprintf('Mode = %s', mode_);
 
-[Ma_est, Ma_err] = SLD4M( chisq(idx_sumstat), ell2, ell4, idx_ref, annot, EW2(idx_ref), EW4(idx_ref), no_blocks, 1, report_annot_mat);
+[ Ma_est, Ma_err, ...
+  h2_est, h2_err, ...
+  Maenrich_est, Maenrich_err, ...
+  h2enrich_est, h2enrich_err, ...
+  ma_jk, h2_jk, ...
+  kurtexp_est, kurtexp_err, ...
+  propkurt_est, propkurt_err ] = SLD4M( chisq(idx_sumstat), ell2, ell4, idx_ref, annot, EW2(idx_ref), EW4(idx_ref), no_blocks, ref_idx, report_annot_mat);
 
-out_table = table([ "Manual_aggregated", Annot_names' ]', Ma_est, Ma_err);
+if strcmp(mode_, 'base') | strcmp(mode_, 'all')
+  outnames = Annot_names;
+elseif strcmp(mode_, 'all_w_agg')
+  outnames = [ "Manual_aggregated", Annot_names' ]';
+end 
+
+out_table = table(outnames, ...
+  Ma_est, Ma_err, ...
+  h2_est, h2_err, ...
+  Maenrich_est, Maenrich_err, ...
+  h2enrich_est, h2enrich_err);
 writetable(out_table, out_file, 'Delimiter', ',');
 
 exit
